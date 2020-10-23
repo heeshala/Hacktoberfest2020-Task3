7f8733cfc13e0965bb3fcae75e6da1b2dafded73
shehancg <shehanchamik@gmail.com>
2020-10-23 14:18:28 +0530

    Update Stack.c
    I corrected the errors in the C code

--- Stack.c
+++ Stack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 //Global Variable
-int size,choice,ele;
+int size,choice,ele,i;
 
 
 //Creating Stack
@@ -14,15 +14,15 @@ struct stack{
 //Inserting Element
 void push(int element)
 {
-    if((st.top)==size)
+    if((st.top)==size-1)
     {
         printf("\n Stack is Full");
     }
     else
     {
-        st.top--;
+        st.top++;
         printf("\nEnter a Value ");
-        scanf("%s",&ele);
+        scanf("%d",&ele);
         st.arr[st.top]=ele;
     }
 }
@@ -38,7 +38,7 @@ int pop()
     {
         int out;
         out=st.arr[st.top];
-        st.top++;
+        st.top--;
         return out;
     }
 }
@@ -47,7 +47,7 @@ int pop()
 int peek()
 {
     int display;
-    display=st.arr[st.rear];
+    display=st.arr[st.top];
     return display;
 }
 
@@ -57,7 +57,7 @@ void display()
     if((st.top)>=0)
     {
         printf("\n\nElements in the Stack");
-        for(i=st.top;i>=0;i++)
+        for(i=st.top;i>=0;i--)
         {
             printf("\n%d",st.arr[i]);
         }
@@ -78,7 +78,7 @@ int main()
 
     do{
         printf("\nEnter Your Choice  ");
-        scanf("%c",&choice);
+        scanf("%d",&choice);
         switch(choice)
         {
         case 1:
@@ -87,11 +87,11 @@ int main()
             }
         case 2:
             {
-                printf("%d",pop());
+                printf("%d",pop());break;
             }
         case 3:
             {
-                printf("%d",peek());
+                printf("%d",peek());break;
             }
         case 4:
             {
