a650beb287a1140dc906412900b12d5808faba74
Rashmildp <64850016+Rashmildp@users.noreply.github.com>
2020-10-24 12:09:42 +0530

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -22,7 +22,7 @@ void push(int element)
     {
         st.top--;
         printf("\nEnter a Value ");
-        scanf("%s",&ele);
+        scanf("%d",&ele);
         st.arr[st.top]=ele;
     }
 }
@@ -32,6 +32,7 @@ int pop()
 {
     if((st.top)==-1)
     {
+        
         printf("\nStack is Empty");
     }
     else
@@ -47,7 +48,7 @@ int pop()
 int peek()
 {
     int display;
-    display=st.arr[st.rear];
+    display=st.arr[st.top];
     return display;
 }
 
@@ -57,7 +58,7 @@ void display()
     if((st.top)>=0)
     {
         printf("\n\nElements in the Stack");
-        for(i=st.top;i>=0;i++)
+        for(int i=st.top;i>=0;i++)
         {
             printf("\n%d",st.arr[i]);
         }
@@ -78,7 +79,7 @@ int main()
 
     do{
         printf("\nEnter Your Choice  ");
-        scanf("%c",&choice);
+        scanf("%d",&choice);
         switch(choice)
         {
         case 1:
